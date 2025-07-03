ad9484062a9ee09001f79ba2f63916d9a2193dc3
Jason <jason@vanfreckle.com>
2025-07-03 18:38:57 -0500

    Terraform: separated main.tf into mutliple modularized files

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "web" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "HomelabEC2SSH"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              set -ex
+
+              # Update and install Docker
+              dnf update -y
+              dnf install docker -y
+
+              # Start and enable Docker
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ec2-user
+
+              # Run your container (fallback to Nginx if custom fails)
+              docker run -d -p 80:80 --name capstone-app coruscantsunrise/capstone-nginx:latest 
+
+              EOF
+
+  tags = {
+    Name = "capstone-web"
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "capstone-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "capstone-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "capstone-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "capstone-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
